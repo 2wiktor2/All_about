00d0f2087e5c75f14670890d50345239a0e2ea3f
Wiktor22 <wiktor@bk.ru>
2018-11-22 01:51:40 +0300

    toStringValueOfParseInt

---
+++ src/com/company/Math.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Math {
+    public static void main(String[] args) {
+
+        // Метод Math.max()
+
+
+        //Метод Math.max() — возвращает максимальное значение из двух аргументов.
+        // Аргумент может быть int, float, long, double.
+
+/*        System.out.println(Math.max(1.123, 1.456));
+        System.out.println(Math.max(2.12, 2.0));
+        System.out.println(Math.max(-5.2, -5.1));
+        System.out.println(Math.max(0, -1.1));           */
+
+    }
+}
